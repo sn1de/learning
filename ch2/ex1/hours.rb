21f6e7dd3f5518c64b86f647de37573a0828ebb9
Brad Schneider <bschneider@containerstore.com>
2013-06-24 11:56:20 -0500

    Implemented all the chapter 2 exercises.
    There are 4 exercises in the chapter. I implemented
    all of them. I also added a hash-bang directive
    so it can execute as a shell script without specifying
    the interpreter.

--- ch2/ex1/hours.rb
+++ ch2/ex1/hours.rb
@@ -1,1 +1,13 @@
-puts "I'm going to calculate the number of hours in the year..."
+#!/usr/bin/env ruby
+
+puts "Hours in a year:"
+puts 24 * 365
+
+puts "Minutes in a decade:"
+puts 10*365*24*60
+
+puts "My Age in Seconds:"
+puts (43*365*24*60*60)
+
+puts "Author's Age:"
+puts ((((1111000000/60)/60)/24)/365)
